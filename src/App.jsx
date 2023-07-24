675e8ab731dfe382d145730a5ee54b53ea1cffd0
MarianoBarberi <marianoawswm@hotmail.com>
2023-07-23 20:28:09 -0600

    categoryviewtry

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,53 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import NavScreen from './pages/navigationScreen';
 import './App.css';
-import VidScout from './pages/vidScout';
-import VidSteel from './pages/vidSteel';
-import Drills from './pages/drills';
-import AcFisico from './pages/acFisico';
-import PlayBook from './pages/playbook';
-import DocCoach from './pages/docCoach';
-import DocAdm from './pages/docAdm';
-import MostrarVideo from './pages/mostrarVideo';
+
+import CategoryView from './pages/CategoryView';
+import navigation from './data/navigation.json';
 
 export default function App() {
   return (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<NavScreen/>} />
-            <Route path="/videosScout" element={<VidScout />} />
-            <Route path="/videosSteelers" element={<VidSteel />} />
-            <Route path="/drills" element={<Drills />} />
-            <Route path="/acondicionamientoFisico" element={<AcFisico />} />
-            <Route path="/playbook" element={<PlayBook />} />
-            <Route path="/documentosCoaches" element={<DocCoach />} />
-            <Route path="/documentosAdministracion" element={<DocAdm />} />
-            <Route path="/video" element={<MostrarVideo />} />
+
+            <Route path="scout">
+              <Route index element={<CategoryView viewData={navigation.scout}/>}/>
+              <Route path="ofensa" element={<CategoryView viewData={navigation.ofensa}/>}/>
+              <Route path="defensa" element={<CategoryView viewData={navigation.defensa}/>}/>
+              <Route path="especiales" element={<CategoryView viewData={navigation.especiales}/>}/>
+            </Route>
+
+            <Route path="steelers">
+              <Route index element={<CategoryView viewData={navigation.steelers}/>}/>
+              <Route path="ofensa" element={<CategoryView viewData={navigation.ofensa}/>}/>
+              <Route path="defensa" element={<CategoryView viewData={navigation.defensa}/>}/>
+              <Route path="especiales" element={<CategoryView viewData={navigation.especiales}/>}/>
+            </Route>
+
+            <Route path="drills">
+              <Route index element={<CategoryView viewData={navigation.drills}/>}/>
+            </Route>
+
+            <Route path="acondicionamiento">
+              <Route index element={<CategoryView viewData={navigation.acondicionamiento}/>}/>
+              <Route path="ligas" element={<CategoryView viewData={navigation.ligas}/>}/>
+              <Route path="gym" element={<CategoryView viewData={navigation.gym}/>}/>
+            </Route>
+
+            <Route path="playbook">
+              <Route index element={<CategoryView viewData={navigation.playbook}/>}/>
+            </Route>
+
+            <Route path="coaches">
+              <Route index element={<CategoryView viewData={navigation.coaches}/>}/>
+            </Route>
+
+            <Route path="administracion">
+              <Route index element={<CategoryView viewData={navigation.administracion}/>}/>
+            </Route>
+
           </Routes>
         </BrowserRouter>
   );
 }
-
-// npm install react-router-dom
