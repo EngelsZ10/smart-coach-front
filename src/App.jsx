e1b130207675825b950e03f2f48e8763a02efd19
MarianoBarberi <marianoawswm@hotmail.com>
2023-07-26 18:21:41 -0600

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import NavScreen from './pages/navigationScreen';
 import './App.css';
-import VidScout from './pages/vidScout';
-import VidSteel from './pages/vidSteel';
-import Drills from './pages/drills';
-import AcFisico from './pages/acFisico';
-import PlayBook from './pages/playbook';
-import DocCoach from './pages/docCoach';
-import DocAdm from './pages/docAdm';
-import MostrarVideo from './pages/mostrarVideo';
+
+import Login from './Login';
 
 export default function App() {
   return (
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<NavScreen/>} />
-            <Route path="/videosScout" element={<VidScout />} />
-            <Route path="/videosSteelers" element={<VidSteel />} />
-            <Route path="/drills" element={<Drills />} />
-            <Route path="/acondicionamientoFisico" element={<AcFisico />} />
-            <Route path="/playbook" element={<PlayBook />} />
-            <Route path="/documentosCoaches" element={<DocCoach />} />
-            <Route path="/documentosAdministracion" element={<DocAdm />} />
-            <Route path="/video" element={<MostrarVideo />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<NavScreen/>} />
+            <Route path="/videosScout" />
+            <Route path="/videosSteelers"  />
+            <Route path="/drills"  />
+            <Route path="/acondicionamientoFisico"/>
+            <Route path="/playbook"  />
+            <Route path="/documentosCoaches"/>
+            <Route path="/documentosAdministracion"/>
+            <Route path="/video"/>
           </Routes>
         </BrowserRouter>
   );
