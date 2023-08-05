62769217e80572f251cee11d2da182c9476cb369
EngelsZ10 <62487204+EngelsZ10@users.noreply.github.com>
2023-08-05 01:28:55 -0600

    upload pdf

--- src/pages/DocumentViews.jsx
+++ src/pages/DocumentViews.jsx
@@ -1,120 +1,171 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { Group, Text } from "@mantine/core";
+import { useLocation, useNavigate, Link } from "react-router-dom";
+import {
+  LoadingOverlay,
+  Button,
+  Popover,
+  Group,
+  Text,
+  Dialog,
+} from "@mantine/core";
 import "./CategoryView.css";
-// import { IconUpload, IconFileTypePdf, IconX } from "@tabler/icons-react";
+import { IconUpload, IconX } from "@tabler/icons-react";
 import { Dropzone, PDF_MIME_TYPE } from "@mantine/dropzone";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
 
 function CategoryView({ viewData }, props) {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const title = getTitle(viewData, location.pathname);
-  const theme = getTheme(viewData, location.pathname);
+  const title = viewData.title;
+  const theme = viewData.theme;
   const buttonContent = getButtonContent(location.pathname);
   const columns = viewData.columns || 3;
   const items = viewData.items;
 
   //const stelscout = window.location.pathname.split("/").slice(-2)[0];
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const [popoverOpened] = useState(() => {
+    var obj = {};
+    viewData.items.forEach((items) => {
+      obj[items.category] = false;
+    });
 
+    return obj;
+  });
+  const [visible, handlers] = useDisclosure(false);
+  const [file, setfile] = useState("");
   return (
-    <div className={`view view--${theme}`}>
-      <header className="view_header">
-        <div className="container">
-          <h3 className="header_title">{title}</h3>
-          <button className="header_button" onClick={() => navigate(-1)}>
-            <i className="fa fa-solid fa-arrow-left"></i>
-            Volver a {buttonContent}
-          </button>
-        </div>
-        <figure className="header_logo">
-          <img src="/Logos/Logo circular1.png" alt="Logo Circular 1" />
-          <img src="/Logos/logo texto1.png" alt="Logo texto 1" />
-        </figure>
-      </header>
-      <section className={`subcategory-list cols--${columns}`}>
-        {items.map((item) => (
-          <div className="nav-item" key={item.category}>
-            <div className="pdfcase">
-              <div className="pdfview">
-                <Dropzone
-                  onDrop={(files) => console.log("accepted files", files)}
-                  onReject={(files) => console.log("rejected files", files)}
-                  maxSize={3 * 1024 ** 2}
-                  accept={PDF_MIME_TYPE}
-                  {...props}
-                >
-                  <Group position="center" spacing="xl" className="Grupo">
-                    <Dropzone.Accept>
-                      {/* <IconUpload
-                        size="3.2rem"
-                        stroke={1.5}
-                        color={
-                          theme.colors[theme.primaryColor][
-                            theme.colorScheme === "dark" ? 4 : 6
-                          ]
-                        }
-                      /> */}
-                    </Dropzone.Accept>
-                    <Dropzone.Reject>
-                      {/* <IconX
-                        size="3.2rem"
-                        stroke={1.5}
-                        color={
-                          theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]
-                        }
-                      /> */}
-                    </Dropzone.Reject>
-                    <Dropzone.Idle>
-                      {/* <IconFileTypePdf size="3.2rem" stroke={1.5} /> */}
-                      <figure className="item_image">
-                        <img
-                          src={item.image}
-                          alt={`category-${item.category}`}
-                        />
-                      </figure>
-                    </Dropzone.Idle>
-                  </Group>
-                </Dropzone>
-              </div>
-            </div>
-
-            <Link to={item.target}>
-              <Text className="pdfname item_category">{item.category}</Text>
-            </Link>
+    <>
+      <div className={`view view--${theme}`}>
+        <header className="view_header">
+          <div className="container">
+            <h3 className="header_title">{title}</h3>
+            <button className="header_button" onClick={() => navigate(-1)}>
+              <i className="fa fa-solid fa-arrow-left"></i>
+              Volver a {buttonContent}
+            </button>
           </div>
-        ))}
-      </section>
-    </div>
+          <figure className="header_logo">
+            <img src="/Logos/Logo circular1.png" alt="Logo Circular 1" />
+            <img src="/Logos/logo texto1.png" alt="Logo texto 1" />
+          </figure>
+        </header>
+        <section className={`subcategory-list cols--${columns}`}>
+          {items.map((item) => (
+            <div className="nav-item" key={item.category}>
+              <div className="pdfcase">
+                <div className="pdfview">
+                  <Dropzone
+                    onDrop={(files) => {
+                      popoverOpened[item.category] = true;
+                      setfile(files[0]);
+                    }}
+                    onReject={() => {
+                      toggle();
+                      setTimeout(() => {
+                        close();
+                      }, 1000);
+                    }}
+                    maxSize={3 * 1024 ** 2}
+                    accept={PDF_MIME_TYPE}
+                    {...props}
+                  >
+                    <Group position="center" spacing="xl" className="Grupo">
+                      <Dropzone.Accept>
+                        <IconUpload size="8rem" stroke={3} color="Green" />
+                      </Dropzone.Accept>
+                      <Dropzone.Reject>
+                        <IconX size="8rem" stroke={3} color="red" />
+                      </Dropzone.Reject>
+                      <Dropzone.Idle>
+                        {/* <IconFileTypePdf size="3.2rem" stroke={1.5} /> */}
+                        <figure className="item_image">
+                          <img
+                            src={item.image}
+                            alt={`category-${item.category}`}
+                          />
+                        </figure>
+                      </Dropzone.Idle>
+                    </Group>
+                  </Dropzone>
+                </div>
+              </div>
+              <Popover
+                opened={popoverOpened[item.category]}
+                width={250}
+                position="bottom"
+                withArrow
+                shadow="md"
+              >
+                <Link to={item.target}>
+                  <Popover.Target>
+                    <Text className="pdfname item_category">
+                      {item.category}
+                    </Text>
+                  </Popover.Target>
+                </Link>
+
+                <Popover.Dropdown>
+                  <LoadingOverlay visible={visible} overlayBlur={2} />
+                  <Text size="sm" style={{ padding: 10 }}>
+                    Seguro que quieres remplazar el archivo {item.category} con{" "}
+                    {file.name}
+                  </Text>
+                  <span></span>
+                  <Group position="right" spacing="xs" grow>
+                    <Button
+                      color="green"
+                      size="sm"
+                      onClick={() => {
+                        handlers.toggle();
+                        var formdata = new FormData();
+                        formdata.append("File", file, file.name);
+                        var requestOptions = {
+                          method: "POST",
+                          body: formdata,
+                          redirect: "follow",
+                        };
+
+                        fetch(
+                          `http://localhost:8080/myapp/uploadFile.php?dir=/datos/&filename=${item.category}`,
+                          requestOptions
+                        )
+                          .then((response) => {
+                            popoverOpened[item.category] = false;
+                            setfile("");
+                            handlers.toggle();
+                            return response.text();
+                          })
+                          .then((result) => console.log(result))
+                          .catch((error) => console.log("error", error));
+                      }}
+                    >
+                      enviar
+                    </Button>
+                    <Button color="red" size="sm" onClick={() => {}}>
+                      cancelar
+                    </Button>
+                  </Group>
+                </Popover.Dropdown>
+              </Popover>
+            </div>
+          ))}
+        </section>
+      </div>
+      <Dialog
+        opened={opened}
+        withCloseButton
+        onClose={close}
+        size="lg"
+        radius="md"
+      >
+        <Text size="sm" mb="xs" weight={500}>
+          No se pudo cargar el archivo
+        </Text>
+      </Dialog>
+    </>
   );
-}
-
-function getTitle(viewData, url) {
-  if (viewData.title === "parent") {
-    const title = url.replace(/[-]/g, " ");
-    return title;
-  }
-  return viewData.title;
-}
-
-function getTheme(viewData, url) {
-  if (viewData.theme === "parent") {
-    const view = url.split("/")[1];
-    let theme;
-    switch (view) {
-      case "scout":
-        theme = "yellow";
-        break;
-      case "steelers":
-        theme = "black";
-        break;
-      default:
-        theme = "white";
-        break;
-    }
-    return theme;
-  }
-  return viewData.theme;
 }
 
 function getButtonContent(url) {
