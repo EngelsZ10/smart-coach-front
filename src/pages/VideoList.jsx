d220b1c77cb412aa61ca435243f7497718f47fc0
EngelsZ10 <62487204+EngelsZ10@users.noreply.github.com>
2023-08-06 00:01:27 -0600

    agrega cargar imagenes

--- src/pages/VideoList.jsx
+++ src/pages/VideoList.jsx
@@ -3,17 +3,24 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { useDisclosure } from "@mantine/hooks";
 import { Center, Group, Button } from "@mantine/core";
 import Demo from "../Components/uploadPage";
+import { Dropzone } from "@mantine/dropzone";
+
 import MostrarVideo from "./mostrarVideo";
 
 import "./VideoList.css";
+import { render } from "@testing-library/react";
 
 function VideoList() {
+  const [image, setImage] = useState(
+    "https://e2.365dm.com/21/09/2048x1152/skysports-nfl-logo-badge_5530522.jpg"
+  );
+  const [able, setAble] = useState(false);
   const [videos, setVideos] = useState([]);
   const [searchParams] = useSearchParams();
 
   const equipo = searchParams.get("equipo");
   const categoria = searchParams.get("categoria");
-  //const tipo = searchParams.get('tipo');
+  const tipo = searchParams.get("tipo");
 
   const navigate = useNavigate();
 
@@ -21,6 +28,7 @@ function VideoList() {
 
   function Upload() {
     const [opened, { toggle, close }] = useDisclosure(false);
+
     return (
       <>
         <div className="App">
@@ -37,11 +45,10 @@ function VideoList() {
   }
 
   function getTheme() {
-    if (equipo) {
-      return equipo === "steelers" ? "black" : "yellow";
-    }
     let theme;
-    switch (categoria) {
+    switch (equipo) {
+      case "steelers":
+        theme = "black";
       case "drills":
         theme = "white";
         break;
@@ -49,18 +56,12 @@ function VideoList() {
         theme = "red";
         break;
       default:
+        theme = "yellow";
         break;
     }
 
     return theme;
   }
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const files = e.dataTransfer.files;
-    console.log(files);
-    // TODO: Save files on server
-  };
 
   // TODO: Connect to db to retrieve real videos
   function fetchVideos() {
@@ -125,23 +126,99 @@ function VideoList() {
           <div className="video" key={video.id}>
             <img
               className="video_thumbnail"
-              src={video.thumbnailUrl}
+              src={image /* video.thumbnailUrl */}
               alt={video.name}
             />
             <div className="item_category">{video.name}</div>
           </div>
         ))}
       </div>
-      <div
-        className="drag-drop"
-        onDrop={handleDrop}
-        onDragOver={(e) => e.preventDefault()}
+      <Dropzone
+        loading={able}
+        onDrop={(files) => {
+          var file = files[0];
+          var formdata = new FormData();
+          var requestOptions = {
+            method: "POST",
+            redirect: "follow",
+          };
+          console.log(file.type);
+          if (files[0].type.includes("image/")) {
+            console.log("upload image");
+            var reader = new FileReader();
+            reader.onloadend = function () {
+              console.log("onload");
+              formdata.append("File", files[0]);
+              formdata.append("Image", reader.result);
+              requestOptions.body = formdata;
+              var team = localStorage.getItem("equipo");
+              var dir = tipo ? "/" + tipo : "";
+
+              fetch(
+                `http://localhost:8080/myapp/uploadFile.php?dir=public_html/back/datos/${team}/${equipo}/${categoria}${dir}&filename=${files[0].name}`,
+                requestOptions
+              )
+                .then((response) => response.text())
+                .then((result) => console.log(result))
+                .catch((error) => console.log("error", error));
+            };
+            reader.readAsDataURL(file);
+            return;
+          }
+
+          if (!files[0].type.includes("video/")) return;
+
+          setAble(true);
+          var url = URL.devDependencies(file);
+          console.log(url);
+
+          var video = document.createElement("video");
+          video.src = url;
+          video.currentTime = 3;
+
+          var snapshot = function () {
+            var canvas = document.createElement("canvas");
+            var ctx = canvas.getContext("2d");
+
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+            formdata.append("File", files[0]);
+            formdata.append("Image", canvas.toDataURL("image/png"));
+            setImage(canvas.toDataURL("image/png"));
+            requestOptions.body = formdata;
+
+            /*  fetch(
+              "http://localhost:8080/myapp/uploadFile.php?dir=public_html/back/datos/&filename=archivo",
+              requestOptions
+            )
+              .then((response) => response.text())
+              .then((result) => console.log(result))
+              .catch((error) => console.log("error", error)); */
+            video.removeEventListener("canplay", snapshot);
+            setAble(false);
+          };
+          video.addEventListener("canplay", snapshot);
+        }}
+        style={{
+          backgroundColor: "transparent",
+          border: 0,
+        }}
       >
-        Arrastra un video a esta zona para añadirlo a la lista.
-      </div>
-      <MostrarVideo />
+        <Dropzone.Idle>
+          <div className="drag-drop">
+            Arrastra un video a esta zona para añadirlo a la lista.
+          </div>
+        </Dropzone.Idle>
+        <Dropzone.Accept>
+          <div className="drag-drop">Agregar archivo.</div>
+        </Dropzone.Accept>
+        <Dropzone.Reject>
+          <div className="drag-drop">Archivo no compatible</div>
+        </Dropzone.Reject>
+      </Dropzone>
     </div>
   );
 }
+//<MostrarVideo />
 
 export default VideoList;
